92d633208131762dde45493c2f0e11dcfcb5e3ef
Juhi Chaudhari <juhichaudhari@gmail.com>
2021-04-20 02:26:31 +0000

    small change

--- app.py
+++ app.py
@@ -137,6 +137,5 @@ if __name__ == '__main__':
     app.run(
         port = int(os.getenv('PORT', 8080)),
         host = os.getenv("IP", '0.0.0.0'),
-        use_reloader=True,
         debug  = True
     )
